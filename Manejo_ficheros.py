550d8b1962080dbb792caa9c73c584285db4f853
homerj31 <40171213+homerj31@users.noreply.github.com>
2025-06-20 12:25:16 +0000

    mejoras

--- Manejo_ficheros.py
+++ Manejo_ficheros.py
@@ -1,6 +1,8 @@
-import csv
+import csv, os
 
 total = 0.0
+
+os.system ("clear")
 
 def es_numero(valor):
     try:
@@ -9,11 +11,12 @@ def es_numero(valor):
     except ValueError:
         return False
 
-with open("Day54Totals.csv", "w+", encoding="utf-8") as file:
+with open("Day54Totals.csv", "r", encoding="utf-8") as file:
     reader = csv.DictReader(file)
     for row in reader:
         if (es_numero(row["Quantity"]) and es_numero(row["Cost"])):
-            total += float(row["Quantity"]) * float(row["Cost"])
+            total += int(row["Quantity"]) * float(row["Cost"])
+            print (f"Del producto {row["Name"]} tenemos {row["Quantity"]} unidades y cada una cuesta {row["Cost"]}")
         else:
             print(f"Invalid data in row: {row}")
 
