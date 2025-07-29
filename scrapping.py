4c9983266e8f7ecbd0f6d2c28662a6fd30f39044
homerj31 <40171213+homerj31@users.noreply.github.com>
2025-07-29 11:54:56 +0200

    Create scrapping.py

---
+++ scrapping.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import requests
+
+def scrape_url(url: str):
+    headers = {
+      'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/131.0.0 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        print('La petición fue exitosa')
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+      # Extraer todos los
+        titulos = [titulo.string for titulo in soup.find_all('h1')]
+      # print(titulos)
+
+      # Extraer todos los enlaces <a>
+        enlaces = [urljoin(url, enlace.get('href')) for enlace in soup.find_all('a')]
+
+      
+        og_image = soup.find('meta', property='og:image')
+        if og_image:
+            print(og_image['content'])
+        else:
+            print('No se encontró la imagen')
+            
+def html_extract (url: str):
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print('La petición fue exitosa')
+
+        html = response.text
+        print(html)            
+
+#scrape_url('http://petiteteenagergalleries.com/galleries')
+
+html_extract('http://petiteteenagergalleries.com/galleries')
